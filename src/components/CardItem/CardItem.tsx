dbcf5e8ebb37308f4e00523f10942aa04c551c52
fernando-pulido <fernandopulido95@icloud.com>
2024-05-10 22:36:11 -0500

    edit CatdItem.css

--- src/components/CardItem/CardItem.tsx
+++ src/components/CardItem/CardItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Card, Col, Row, Space } from 'antd'
+import { Card, Col } from 'antd'
 
+import './CardItem.css'
 type CardProps = {
   title: string
   numberClients: number
@@ -10,9 +11,9 @@ const Cards: CardProps[] = [
   { title: 'Total de clientes', numberClients: 150 },
   { title: 'Clientes nuevos', numberClients: 10 },
   { title: 'Clientes activos', numberClients: 100 },
-  { title: 'Total de clientes', numberClients: 150 },
-  { title: 'Clientes nuevos', numberClients: 10 },
-  { title: 'Clientes activos', numberClients: 100 },
+  { title: 'Proximos a vencer ', numberClients: 150 },
+  { title: 'Clientes a credito', numberClients: 10 },
+  { title: 'Cumpleaños de clientes ', numberClients: 100 },
 ]
 
 const CardItem: React.FC<CardProps> = ({ title, numberClients }) => (
@@ -22,16 +23,13 @@ const CardItem: React.FC<CardProps> = ({ title, numberClients }) => (
 )
 
 export const CardsItems = () => (
-  <>
-    <Row gutter={16}>
-      {Cards.map((card, index) => (
-        <Col span={8} key={index}>
-          <CardItem key={index} {...card} />
-        </Col>
-      ))}
-    </Row>
-    <Space size="large" />
-  </>
+  <div className="cards-container">
+    {Cards.map((card, index) => (
+      <Col span={8} key={index}>
+        <CardItem key={index} {...card} />
+      </Col>
+    ))}
+  </div>
 )
 
 export default CardItem
